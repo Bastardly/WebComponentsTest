5ab62581b09ffe25088dad4e15461ae4b1803073
Flemming Knøss Hansen <fha@kmd.dk>
2024-04-10 13:32:34 +0200

    Add JSDoc comment to test web component

--- src/modules/blue-text.ts
+++ src/modules/blue-text.ts
@@ -1,6 +1,9 @@
 import { ShadowElement, wcDefine } from "@app/core/wcshadow";
 
-
+/**
+ * This is a very simple component made for testing purposes that should be easy to understand.
+ * Normally, there would be no need to use ShadowDom for this type of Component.
+ */
 wcDefine('blue-text', class extends ShadowElement {
 	p = document.createElement('p');
 
